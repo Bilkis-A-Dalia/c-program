70dbe9704a3a1d08e5210bf3f9e809f361e49a72
BilkisAkterDalia <bilkisadalia@gmail.com>
2023-04-02 20:40:57 +0600

    limitaton.c

---
+++ module1/limitation.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=1000000000;
+    long long int b=100000000000;
+    float c=2.5;
+    double d=2005.745864358;
+    printf("%d %lld %.1f %lf",a, b, c, d);
+    return 0;
+}
---
+++ module1/print.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello world\n");
+    Printf("Now i am a programmer");
+    return 0;
+}
+
+//Hello world
+//Now i am a programmer
