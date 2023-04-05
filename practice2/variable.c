bbd6ccc9dc4f6370490a9277773a962f9e1e957c
BilkisAkterDalia <bilkisadalia@gmail.com>
2023-04-06 05:37:55 +0600

    practice.c

---
+++ practice2/variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    long long int b;
+    float m;
+    char  n;
+    scanf("%d %lld %f %c",&a,&b,&m,&n);
+    printf("%d \n%lld \n%.2f \n%c",a,b,m,n); 
+    return 0;
+}
