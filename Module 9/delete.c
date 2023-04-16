0bd352f5b73dc76711696778baee7cc065235166
BilkisAkterDalia <bilkisadalia@gmail.com>
2023-04-16 12:55:15 +0600

    module 9

---
+++ Module 9/delete.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int ar[n+1];
+    for ( i=0; i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    int position,value;
+    scanf("%d %d",&position,&value);
+    for (i=n;i>=position+1;i--)
+    {
+        ar[i]=ar[i-1];
+    }
+    ar[position]=value;
+    for (i=0;i<=n;i++)
+    {
+        printf("%d ",ar[i]);
+    }
+    return 0;
+}
